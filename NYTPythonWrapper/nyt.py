b709dfa876df6ae39ef8be5e64f8ec6716d746f6
Tiffany Lee <tl2606@barnard.edu>
2019-03-11 22:42:42 -0400

    Add main class and some setup related files

---
+++ NYTPythonWrapper/nyt.py
@@ -0,0 +1,37 @@
+import requests
+import json
+
+API_ROOT = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?'
+
+
+class NoKeyException:
+    def __init__(self, error):
+        self.error = error
+
+    def __str__(self):
+        return repr(self.error)
+
+
+class NoResponseException:
+    def __init__(self, query, response):
+        self.response = response
+
+    def __str__(self):
+        return repr('GET %s %s' % (self.query, self.response))
+
+
+class NYTSearch(object):
+    def __init__(self, key=None):
+        self.key = key
+        if key is None:
+            raise NoKeyException('')
+
+    def search(self, terms):
+        terms_formatted = 'q=' + terms.replace(' ', '+')
+        api_key = '&api-key='+self.key
+        query = API_ROOT + terms_formatted + api_key
+        response = requests.get(query)
+        if response.status_code != 200:
+            raise NoResponseException(query, response.status_code)
+        else:
+            return response.json()
---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='NYTPythonWrapper',
+      version='0.1',
+      description='My version of NYT python wrapper',
+      url='',
+      author='Tiffany Lee',
+      packages=['NYTPythonWrapper'])
